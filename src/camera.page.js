39d486923799a0a0fb80e359369af6bf1c017d54
Sachin Srivastava <sachinsrivastava0126@gmail.com>
2019-12-11 20:27:37 -0500

    Taking pictures now.

--- src/camera.page.js
+++ src/camera.page.js
@@ -2,15 +2,41 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import * as Permissions from 'expo-permissions';
-import {Camera} from 'expo-camera';
+import { Camera } from 'expo-camera';
+import Toolbar from './toolbar.component';
+import Gallery from './gallery.component';
 
 import styles from './styles';
 
 export default class CameraPage extends React.Component {
     camera = null;
-
     state = {
+        captures: [],
+        // setting flash to be turned off by default
+        flashMode: Camera.Constants.FlashMode.off,
+        capturing: null,
+        // start the back camera by default
+        cameraType: Camera.Constants.Type.back,
         hasCameraPermission: null,
+    };
+
+    setFlashMode = (flashMode) => this.setState({ flashMode });
+    setCameraType = (cameraType) => this.setState({ cameraType });
+    handleCaptureIn = () => this.setState({ capturing: true });
+
+    handleCaptureOut = () => {
+        if (this.state.capturing)
+            this.camera.stopRecording();
+    };
+
+    handleShortCapture = async () => {
+        const photoData = await this.camera.takePictureAsync();
+        this.setState({ capturing: false, captures: [photoData, ...this.state.captures] })
+    };
+
+    handleLongCapture = async () => {
+        const videoData = await this.camera.recordAsync();
+        this.setState({ capturing: false, captures: [videoData, ...this.state.captures] });
     };
 
     async componentDidMount() {
@@ -22,7 +48,7 @@ export default class CameraPage extends React.Component {
     };
 
     render() {
-        const { hasCameraPermission } = this.state;
+        const { hasCameraPermission, flashMode, cameraType, capturing, captures } = this.state;
 
         if (hasCameraPermission === null) {
             return <View />;
@@ -31,12 +57,30 @@ export default class CameraPage extends React.Component {
         }
 
         return (
-            <View>
-                <Camera
-                    style={styles.preview}
-                    ref={camera => this.camera = camera}
+            <React.Fragment>
+                <View>
+                    <Camera
+                        type={cameraType}
+                        flashMode={flashMode}
+                        style={styles.preview}
+                        ref={camera => this.camera = camera}
+                    />
+                </View>
+ 
+                {captures.length > 0 && <Gallery captures={captures}/>}
+                
+                <Toolbar 
+                    capturing={capturing}
+                    flashMode={flashMode}
+                    cameraType={cameraType}
+                    setFlashMode={this.setFlashMode}
+                    setCameraType={this.setCameraType}
+                    onCaptureIn={this.handleCaptureIn}
+                    onCaptureOut={this.handleCaptureOut}
+                    onLongCapture={this.handleLongCapture}
+                    onShortCapture={this.handleShortCapture}
                 />
-            </View>
+            </React.Fragment>
         );
     };
 };
